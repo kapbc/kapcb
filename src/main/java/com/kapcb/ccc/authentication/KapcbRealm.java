2624ab7e133483bdbb324d90b9e787ea1a33989c
kapcb <eircccallroot@yeah.net>
2021-08-05 22:43:10 +0800

    shiro

--- src/main/java/com/kapcb/ccc/authentication/KapcbRealm.java
+++ src/main/java/com/kapcb/ccc/authentication/KapcbRealm.java
@@ -1,4 +1,4 @@
-package com.kapcb.ccc.shiro;
+package com.kapcb.ccc.authentication;
 
 import com.kapcb.ccc.model.po.UserPO;
 import com.kapcb.ccc.service.IPermissionService;
@@ -10,9 +10,17 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.apache.shiro.util.SimpleByteSource;
+
+import javax.annotation.PostConstruct;
+import java.util.Objects;
 
 /**
  * <a>Title: KapcbRealm </a>
@@ -31,6 +39,13 @@ public class KapcbRealm extends AuthorizingRealm {
     private final IUserService userService;
     private final IRoleService roleService;
     private final IPermissionService permissionService;
+
+    @PostConstruct
+    public void initConfigure(){
+        setAuthenticationCachingEnabled(false);
+        setAuthorizationCachingEnabled(true);
+        setCachingEnabled(true);
+    }
 
     /**
      * 授权
@@ -59,7 +74,17 @@ public class KapcbRealm extends AuthorizingRealm {
         if (StringUtils.isNoneBlank(email)) {
             // 可以根据实际情况做缓存，如果不做，Shiro自己也是有时间间隔机制，2分钟内不会重复执行该方法
             UserPO userPO = userService.getUserByUsername(email);
-
+            if (Objects.isNull(userPO)) {
+                throw new IncorrectCredentialsException("username or password error!");
+            }
+            if (userPO.getAvailableStatus()) {
+                throw new LockedAccountException("account is locked!");
+            }
+            // 这里传入的是user对象，比对的是用户名，直接传入用户名也没错，但是在授权部分就需要自己重新从数据库里取权限
+            // 密码
+            // salt
+            // realm name
+            return new SimpleAuthenticationInfo(userPO, userPO.getPassword(), new SimpleByteSource("aaa"), getName());
         }
         return null;
     }
