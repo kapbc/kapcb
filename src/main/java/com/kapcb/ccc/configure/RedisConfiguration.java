e47e3d5d15603ef17ef6257aac1ed0fad2b14e72
kapcb <eircccallroot@yeah.net>
2021-08-13 17:38:53 +0800

    Update RedisConfiguration.java

--- src/main/java/com/kapcb/ccc/configure/RedisConfiguration.java
+++ src/main/java/com/kapcb/ccc/configure/RedisConfiguration.java
@@ -9,5 +9,42 @@ package com.kapcb.ccc.configure;
  * @version 1.0.0
  * @date 2021/7/16 22:32
  */
+@Slf4j
+@Configuration
+@ConditionalOnProperty(prefix = "kapcb.redis", name = "enable", havingValue = "true")
 public class RedisConfiguration {
+  
+  
+      @Bean("redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        log.info("[:::::begin to create redis template:::]");
+        lettuceConnectionFactory.setShareNativeConnection(false);
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build();
+        objectMapper.registerModule(new Jdk8Module());
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        template.setKeySerializer(RedisSerializer.string());
+        template.setHashKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.setConnectionFactory(lettuceConnectionFactory);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    @ConditionalOnBean(name = "redisTemplate")
+    public RedisService redisService(@Qualifier("redisTemplate") RedisTemplate<String, Object> redisTemplate) {
+        log.info("[:::::begin to initial redis service:::::]");
+        return new RedisService(redisTemplate);
+    }
 }
