f85f4788440773ff9dc62257ea43717128d6528b
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-10-08 23:12:02 +0800

    dictionary

--- src/main/java/com/kapcb/ccc/model/po/DictionaryPO.java
+++ src/main/java/com/kapcb/ccc/model/po/DictionaryPO.java
@@ -1,5 +1,11 @@
 package com.kapcb.ccc.model.po;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.Version;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,23 +27,53 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
+@TableName(value = "common_grp.dictionary")
 public class DictionaryPO implements Serializable {
 
     private static final long serialVersionUID = -3216105866722192952L;
 
+    @TableId(type = IdType.AUTO)
+    @TableField(value = "dictionary_id")
     private Integer dictionaryId;
 
-    private String i18nGroup;
+    @TableField(value = "dictionary_code")
+    private String dictionaryCode;
 
-    private String i18nKey;
+    @TableField(value = "dictionary_value_en")
+    private String dictionaryValueEn;
 
-    private String i18nValue;
+    @TableField(value = "dictionary_value_zh")
+    private String dictionaryValueZh;
 
+    @TableField(value = "dictionary_description")
+    private String dictionaryDescription;
+
+    @TableField(value = "dictionary_group")
+    private String dictionaryGroup;
+
+    @TableField(value = "dictionary_remark")
+    private String dictionaryRemark;
+
+    @TableField(value = "dictionary_num")
+    private String dictionaryNum;
+
+    @TableField(value = "create_date")
     private Date createDate;
 
+    @TableField(value = "create_by")
     private Long createBy;
 
+    @TableField(value = "last_update_date")
     private Date lastUpdateDate;
 
+    @TableField(value = "last_update_by")
     private Long lastUpdateBy;
+
+    @TableLogic
+    @TableField(value = "delete_flag")
+    private Boolean deleteFlag;
+
+    @Version
+    @TableField(value = "version")
+    private Integer version;
 }
