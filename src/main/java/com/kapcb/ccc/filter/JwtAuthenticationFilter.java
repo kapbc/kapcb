4bc68f74aa0ab3ff88f6754cd0cc5da89171e7f6
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-10-29 22:56:57 +0800

    authentication

--- src/main/java/com/kapcb/ccc/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/kapcb/ccc/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,16 @@
 package com.kapcb.ccc.filter;
 
 import com.kapcb.ccc.enums.StringPool;
+import com.kapcb.ccc.model.po.UserPO;
+import com.kapcb.ccc.service.IUserService;
 import com.kapcb.ccc.utils.AccessAddressUtil;
-import com.kapcb.ccc.utils.JwtAuthenticationUtil;
+import com.kapcb.ccc.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,6 +19,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Objects;
 
 /**
  * <a>Title: JwtFilter </a>
@@ -30,6 +37,8 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private final IUserService userService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 获取请求头的token
@@ -39,22 +48,37 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         log.info("::::ip address is : {}", ipAddress);
 
         if (StringUtils.isNotBlank(authorization) && authorization.startsWith(StringPool.AUTHORIZATION_BEARER.value())) {
+
             // 从Authorization获取token
             String accessToken = authorization.substring(StringPool.AUTHORIZATION_BEARER.value().length());
             log.info("::::access token is : {}", accessToken);
-            if (StringUtils.isNotBlank(accessToken)) {
-                String username = JwtAuthenticationUtil.parseToken(accessToken);
-                log.info("::::username is : {}", username);
-                if (StringUtils.isNotBlank(username)) {
-
-                    // 检查用户名是否加入黑名单
-
-                    // 判断是否
-                    log.info("::::access success!");
-
-                }
-
+            if (StringUtils.isBlank(accessToken)) {
+                log.error("access token is null or empty!");
             }
+
+            String username = JwtTokenUtil.getUsername(accessToken);
+            if (StringUtils.isNotBlank(username) && Objects.isNull(SecurityContextHolder.getContext().getAuthentication())) {
+                UserPO userByUsername = userService.getUserByUsername(username);
+                if (JwtTokenUtil.validateToken(accessToken, userByUsername)) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userByUsername.getFirstName(), null, Collections.emptyList());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+
+//            if (StringUtils.isNotBlank(accessToken)) {
+//                String username = JwtAuthenticationUtil.parseToken(accessToken);
+//                log.info("::::username is : {}", username);
+//                if (StringUtils.isNotBlank(username)) {
+//
+//                    // 检查用户名是否加入黑名单
+//
+//                    // 判断是否
+//                    log.info("::::access success!");
+//
+//                }
+//
+//            }
         }
         filterChain.doFilter(request, response);
     }
