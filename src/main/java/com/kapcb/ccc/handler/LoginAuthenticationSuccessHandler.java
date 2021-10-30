a74c60b99d9a46974cd98d7365e19e996702a61d
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-10-30 22:25:29 +0800

    result

--- src/main/java/com/kapcb/ccc/handler/LoginAuthenticationSuccessHandler.java
+++ src/main/java/com/kapcb/ccc/handler/LoginAuthenticationSuccessHandler.java
@@ -7,14 +7,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * <a>Title: LoginAuthenticationSuccessHandler </a>
@@ -29,8 +34,20 @@ import java.util.Map;
 @Component
 public class LoginAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+    private final RequestCache requestCache = new HttpSessionRequestCache();
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        SavedRequest savedRequest = requestCache.getRequest(httpServletRequest, httpServletResponse);
+        HttpSession session = httpServletRequest.getSession();
+
+        if (Objects.isNull(session)) {
+            Object attribute = session.getAttribute("SPRING_SECURITY_SAVED_REQUEST");
+            log.info("redirect to user login page's url is {} ", attribute);
+        }
+
+
+
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         String accessToken = JwtTokenUtil.generateToke(userDetails.getUsername());
         httpServletResponse.setContentType(ContentType.JSON.getValue());
---
+++ src/main/java/com/kapcb/ccc/utils/RequestUtil.java
@@ -0,0 +1,20 @@
+package com.kapcb.ccc.utils;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <a>Title: RequestUtil </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: RequestUtil <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/10/30 22:23
+ */
+@Slf4j
+@UtilityClass
+public class RequestUtil {
+
+
+}
