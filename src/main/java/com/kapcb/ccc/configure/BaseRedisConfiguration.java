dc5aaf70cb0db78ba41768c23dfcbad987f72c2a
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-03 22:49:25 +0800

    redis config

---
+++ src/main/java/com/kapcb/ccc/configure/BaseRedisConfiguration.java
@@ -0,0 +1,23 @@
+package com.kapcb.ccc.configure;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * <a>Title: BaseRedisConfiguration </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: BaseRedisConfiguration <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/11/3 22:48
+ */
+@Slf4j
+public class BaseRedisConfiguration {
+
+    @Bean
+//    public RedisTemplate<String, Object> redisTemplate() {
+//
+//    }
+}
