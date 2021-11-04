32ee695f7a1873c511ea6c9cc6b235cb73344fea
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-04 21:08:41 +0800

    mapstruct

--- src/main/java/com/kapcb/ccc/configure/BaseRedisConfiguration.java
+++ src/main/java/com/kapcb/ccc/configure/BaseRedisConfiguration.java
@@ -1,8 +1,6 @@
 package com.kapcb.ccc.configure;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * <a>Title: BaseRedisConfiguration </a>
