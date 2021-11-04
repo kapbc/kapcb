1a5445baa28cac6c8b06a8bb344df1863fe0330c
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-04 22:45:35 +0800

    product

---
+++ src/main/java/com/kapcb/ccc/model/po/product/ProductPO.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.model.po.product;
+
+/**
+ * <a>Title: ProductPO </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: ProductPO <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/11/4 22:45
+ */
+public class ProductPO {
+}
